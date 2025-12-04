8fe7c33e8d86969e1bd4bcf7b98ace88f6042e06
SAWADA Tadashi <cesare@mayverse.jp>
2025-12-04 12:32:26 +0900

    Setup CORS

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,19 @@
-use actix_web::{App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{App, HttpServer, http::header};
+
+fn build_cors() -> Cors {
+    Cors::default()
+        .allowed_origin("http://localhost:3001")
+        .allowed_methods(vec!["POST", "GET", "DELETE", "OPTIONS"])
+        .allowed_headers(vec![header::CONTENT_TYPE])
+        .supports_credentials()
+}
 
 #[actix_rt::main]
 async fn main() -> anyhow::Result<()> {
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(build_cors())
     });
     server.bind(("localhost", 3000))?.run().await?;
     Ok(())
