73a6d21ade3fc5b5907f174b3c0bc1dd8a7a880a
SAWADA Tadashi <cesare@mayverse.jp>
2025-12-04 12:28:34 +0900

    Setup empty http server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+
+#[actix_rt::main]
+async fn main() -> anyhow::Result<()> {
+    let server = HttpServer::new(move || {
+        App::new()
+    });
+    server.bind(("localhost", 3000))?.run().await?;
+    Ok(())
 }
