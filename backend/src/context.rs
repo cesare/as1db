7e41a591f1e175e1b90ae53b806f4d74622929c7
SAWADA Tadashi <cesare@mayverse.jp>
2025-12-05 11:22:37 +0900

    Add config to context

--- backend/src/context.rs
+++ backend/src/context.rs
@@ -1,16 +1,25 @@
 use anyhow::Result;
+use serde::Deserialize;
 use sqlx::{PgPool, Pool, Postgres};
+
+#[derive(Clone, Deserialize)]
+pub struct Config {
+    pub database_url: String,
+}
 
 #[allow(dead_code)]
 #[derive(Clone)]
 pub struct Context {
+    pub config: Config,
     pub pool: Pool<Postgres>,
 }
 
 impl Context {
     pub fn load() -> Result<Self> {
-        let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-        let pool = PgPool::connect_lazy(&database_url)?;
-        Ok(Self { pool })
+        let config = envy::from_env::<Config>()?;
+        let pool = PgPool::connect_lazy(&config.database_url)?;
+
+        let context = Self { config, pool };
+        Ok(context)
     }
 }
