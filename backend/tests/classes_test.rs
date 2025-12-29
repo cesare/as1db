1d45af9040578e6e9614a557109c1731c826b636
SAWADA Tadashi <cesare@mayverse.jp>
2025-12-29 10:26:46 +0900

    Add tests on /classes/:id

--- backend/tests/classes_test.rs
+++ backend/tests/classes_test.rs
@@ -34,3 +34,40 @@ async fn index(pool: PgPool) {
     });
     assert_eq!(response_json, expected_json);
 }
+
+#[sqlx::test(fixtures("classes", "items"))]
+async fn show(pool: PgPool) {
+    let context = common::create_context(pool);
+    let app = test::init_service(
+        App::new()
+            .app_data(Data::new(context))
+            .configure(as1db::handlers::routes),
+    )
+    .await;
+
+    let request = test::TestRequest::get().uri("/classes/1").to_request();
+    let response = test::call_service(&app, request).await;
+
+    assert!(response.status().is_success());
+
+    let response_json: Value = test::read_body_json(response).await;
+    let expected_json = json!({
+        "class": {
+            "id": 1,
+            "name": "category-01",
+        },
+        "items": [
+            {
+                "id": 1,
+                "classId": 1,
+                "name": "item-01",
+            },
+            {
+                "id": 2,
+                "classId": 1,
+                "name": "item-02",
+            },
+        ],
+    });
+    assert_eq!(response_json, expected_json);
+}
---
+++ backend/tests/fixtures/items.sql
@@ -0,0 +1,6 @@
+insert into items (id, class_id, name)
+values
+  (1, 1, 'item-01'),
+  (2, 1, 'item-02'),
+  (3, 2, 'item-03')
+;
